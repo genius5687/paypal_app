55a2e747297ab47d5bed2eb10f6505e950c91f90
Eric Yuan <eric@beecrazy.com>
2011-06-14 17:02:16 +0800

    Improved the README file

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Ruby on Rails Paypal App
+This is the paypal_app for Beecrazy.com (beecrazy.hk)
+by [Eric Yuan](eric@beecrazy.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaypalApp::Application.config.secret_token = 'ab15adc6da6460e7fa47f12f49410d000884beceed82999046eb135a22ac9d3f17bb100f7a21604bfe6620042e15868ad094d016d5f9396391c845b741f94816'
